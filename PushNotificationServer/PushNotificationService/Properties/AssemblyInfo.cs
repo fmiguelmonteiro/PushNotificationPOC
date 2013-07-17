f843480ac30b961bf0edfdb259d00b50b49d14e3
Filipe Monteiro <filipe.monteiro@cision.com>
2013-07-17 14:56:38 +0100

    server: mongo, Push Sharp

---
+++ PushNotificationServer/PushNotificationService/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PushNotificationService")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Cision North America")]
+[assembly: AssemblyProduct("PushNotificationService")]
+[assembly: AssemblyCopyright("Copyright © Cision North America 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("984e3836-979e-49af-b7c8-ecc663824bd2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PushNotificationServer/PushNotificationService/PushNotificationService.svc.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using PushSharp;
+using PushSharp.Android;
+using PushSharp.Apple;
+using PushSharp.Core;
+using MongoDB.Driver;
+
+
+namespace PushNotificationService
+{
+    public class PushNotificationService : IPushNotificationService
+    {
+        public int Register(string regId, string searchTerm)
+        {
+            var client = new MongoClient("mongodb://10.4.0.133");
+            var server = client.GetServer();
+
+            var database = server.GetDatabase("pushNotification");
+            var commentCollection = database.GetCollection("Registers");
+
+            try
+            {
+                var a = commentCollection.Insert(
+                    new { 
+                        regId = regId,  
+                        searchTerm = searchTerm
+                    }, WriteConcern.Acknowledged);
+
+                return 0;
+            }
+            catch (Exception e)
+            {
+                return -1;
+            }
+        }
+
+
+        //public void PushNotification(string apiKey, string regId, string message)
+        //{
+        //    var push = new PushSharp.PushBroker();
+        //    push.RegisterGcmService(new GcmPushChannelSettings("AIzaSyCRdVTZUqfHX7kCQWYAZWYoUXBEEwKZ-kA"));
+        //    push.QueueNotification(new GcmNotification().ForDeviceRegistrationId(regId)
+        //                         .WithJson("{\"message\":\"" + message + " \",\"badge\":7,\"sound\":\"sound.caf\"}"));
+        //}
+
+
+        public List<Message> GetMessages(string searchTerm)
+        {
+            return new List<Message>(){
+                new Message(){
+                    Id = 0,
+                    Title = "Batatas",
+                    Text = "Batatas 0"
+                },
+                new Message(){
+                    Id = 1,
+                    Title = "Batatas",
+                    Text = "Batatas 1"
+                },
+                new Message(){
+                    Id = 2,
+                    Title = "Batatas",
+                    Text = "Batatas 2"
+                },
+            };
+        }
+    }
+}
