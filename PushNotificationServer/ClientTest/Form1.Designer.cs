032c10e97a4c126c21cb80963f5c2d6f4aba55f3
Filipe Monteiro <filipe.monteiro@cision.com>
2013-07-18 17:01:50 +0100

    client to add messages

--- PushNotificationServer/ClientTest/Form1.Designer.cs
+++ PushNotificationServer/ClientTest/Form1.Designer.cs
@@ -31,38 +31,101 @@
             this.send = new System.Windows.Forms.Button();
             this.title = new System.Windows.Forms.TextBox();
             this.text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.url = new System.Windows.Forms.TextBox();
+            this.result = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // send
             // 
-            this.send.Location = new System.Drawing.Point(22, 12);
+            this.send.Location = new System.Drawing.Point(275, 261);
             this.send.Name = "send";
-            this.send.Size = new System.Drawing.Size(75, 23);
+            this.send.Size = new System.Drawing.Size(103, 23);
             this.send.TabIndex = 0;
-            this.send.Text = "Send";
+            this.send.Text = "Send Message";
             this.send.UseVisualStyleBackColor = true;
             this.send.Click += new System.EventHandler(this.button1_Click);
             // 
             // title
             // 
-            this.title.Location = new System.Drawing.Point(22, 41);
+            this.title.Location = new System.Drawing.Point(87, 19);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(250, 20);
+            this.title.Size = new System.Drawing.Size(291, 20);
             this.title.TabIndex = 1;
             // 
             // text
             // 
-            this.text.Location = new System.Drawing.Point(22, 68);
+            this.text.Location = new System.Drawing.Point(87, 46);
             this.text.Multiline = true;
             this.text.Name = "text";
-            this.text.Size = new System.Drawing.Size(250, 104);
+            this.text.Size = new System.Drawing.Size(291, 104);
             this.text.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Title";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "URL";
+            // 
+            // url
+            // 
+            this.url.Location = new System.Drawing.Point(87, 156);
+            this.url.Name = "url";
+            this.url.Size = new System.Drawing.Size(291, 20);
+            this.url.TabIndex = 6;
+            // 
+            // result
+            // 
+            this.result.Location = new System.Drawing.Point(87, 180);
+            this.result.Multiline = true;
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(291, 75);
+            this.result.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(37, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Result";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.ClientSize = new System.Drawing.Size(412, 320);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.url);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.text);
             this.Controls.Add(this.title);
             this.Controls.Add(this.send);
@@ -78,6 +141,12 @@
         private System.Windows.Forms.Button send;
         private System.Windows.Forms.TextBox title;
         private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox url;
+        private System.Windows.Forms.TextBox result;
+        private System.Windows.Forms.Label label4;
     }
 }
 
